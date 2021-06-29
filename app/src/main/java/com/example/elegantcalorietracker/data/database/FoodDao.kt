813a2b3c3ced8bf228902d6eaa6b58921d1bac9a
hahmraro <hahmraro@tuta.io>
2021-06-29 09:47:22 -0300

    Finished documenting FoodDao.kt

--- app/src/main/java/com/example/elegantcalorietracker/data/database/FoodDao.kt
+++ app/src/main/java/com/example/elegantcalorietracker/data/database/FoodDao.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.*
 import androidx.room.OnConflictStrategy.REPLACE
 import com.example.elegantcalorietracker.data.model.Food
+import com.example.elegantcalorietracker.data.model.ListType
 
 /**
  * Data access object used by [FoodDatabase]
@@ -13,8 +14,9 @@ import com.example.elegantcalorietracker.data.model.Food
 @Dao
 interface FoodDao {
     // Suspend methods
+
     @Insert(onConflict = REPLACE)
-    suspend fun insert(foodList: List<Food>)
+    suspend fun insertAll(foodList: List<Food>)
 
     @Insert(onConflict = REPLACE)
     suspend fun insertOne(food: Food)
@@ -25,19 +27,36 @@ interface FoodDao {
     @Delete
     suspend fun delete(food: Food)
 
+    /**
+     * Deletes every food which [ListType] isn't [ListType.HISTORY]
+     */
     @Query("DELETE FROM saved_foods_table WHERE list_type != 4")
-    suspend fun clear()
+    suspend fun clearAllExceptHistory()
 
+    /**
+     * Deletes only the foods which [ListType] is [ListType.HISTORY]
+     */
     @Query("DELETE FROM saved_foods_table WHERE list_type = 4")
     suspend fun clearHistory()
 
+    /**
+     * Retrieves every food which [ListType] isn't [ListType.HISTORY]
+     */
     @Query("SELECT * FROM saved_foods_table WHERE list_type != 4")
     suspend fun getAll(): List<Food>
 
     // Non-suspend methods
+
+    /**
+     * Retrieves all the foods that match the specified [ListType]
+     */
     @Query("SELECT * FROM saved_foods_table WHERE list_type = :listType")
     fun get(listType: Int): LiveData<List<Food>>
 
+    /**
+     * Retrieves the sum of the [Food.calories] of all the foods which
+     * [ListType] isn't [ListType.HISTORY]
+     */
     @Query("SELECT TOTAL(calories) FROM saved_foods_table WHERE list_type != 4")
     fun getKcal(): LiveData<Double>
 }
