0beee012d8b3938233e955579d69630b37369a65
hahmraro <hahmraro@tuta.io>
2021-06-29 10:57:29 -0300

    Documented FoodListAdapter.kt

--- app/src/main/java/com/example/elegantcalorietracker/ui/adapters/FoodListAdapter.kt
+++ app/src/main/java/com/example/elegantcalorietracker/ui/adapters/FoodListAdapter.kt
@@ -11,16 +11,35 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.elegantcalorietracker.R
 import com.example.elegantcalorietracker.data.model.Food
 import com.example.elegantcalorietracker.databinding.ItemFoodBinding
+import com.example.elegantcalorietracker.ui.adapters.FoodListAdapter.FoodViewHolder
 
+/**
+ * Click Listener that manipulates a [Food], used by the [FoodListAdapter]
+ */
 typealias FoodClickListener = (Food) -> Unit
+/**
+ * Long click listener that manipulates a [Food], a [PopupMenu] and a [View],
+ * used by the [FoodListAdapter]
+ */
 typealias FoodLongClickListener = (PopupMenu, Food, View) -> Boolean
 
+/**
+ * [ListAdapter] class used by the app's various [RecyclerView] that displays a
+ * list of [Food] through [FoodViewHolder].
+ *
+ * Can take a [clickListener] and [longClickListener] lambdas that set the
+ * [FoodViewHolder] reaction to user clicks
+ */
 class FoodListAdapter(
     private val clickListener: FoodClickListener? = null,
     private val longClickListener: FoodLongClickListener? = null
 ) :
-    ListAdapter<Food, FoodListAdapter.FoodViewHolder>(DiffCallback) {
+    ListAdapter<Food, FoodViewHolder>(DiffCallback) {
 
+    /**
+     * [RecyclerView.ViewHolder] class that binds the list [Food] name,
+     * calories and serving size to the [ItemFoodBinding]
+     */
     class FoodViewHolder(
         private var binding: ItemFoodBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
@@ -38,8 +57,8 @@ class FoodListAdapter(
     }
 
     /**
-     * Allows the RecyclerView to determine which items have changed when the [List] of
-     * [Food] has been updated.
+     * Allows the RecyclerView to determine which items have changed when the
+     * [List] of [Food] has been updated.
      */
     companion object DiffCallback : DiffUtil.ItemCallback<Food>() {
         override fun areItemsTheSame(
@@ -77,9 +96,11 @@ class FoodListAdapter(
      */
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
         val food = getItem(position)
+        // Sets the item click listener
         if (clickListener != null) {
             holder.itemView.setOnClickListener { clickListener.invoke(food) }
         }
+        // Sets the item long click listener
         if (longClickListener != null) {
             holder.itemView.setOnLongClickListener {
                 val menu = PopupMenu(it.context, it)
@@ -88,6 +109,7 @@ class FoodListAdapter(
                 true
             }
         }
+        // Calls the holder bind method
         holder.bind(food)
     }
 }
